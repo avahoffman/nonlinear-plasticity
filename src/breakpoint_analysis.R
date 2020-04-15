bda6e6531237618cbf485ed21e4bdf0703180cec
Ava Hoffman <avamariehoffman@gmail.com>
2020-04-15 12:58:03 -0400

    Add annotation to breakpoint analysis functions

--- src/breakpoint_analysis.R
+++ src/breakpoint_analysis.R
@@ -12,19 +12,27 @@ get_breakpoints <-
     # df: data frame from which response variable is collected
     # response: response variable (string)
     
+    # Fit glm
     fit <-
       glm(as.formula(paste(response, " ~ trt", sep = "")), data = df)
+    
+    # Find breakpoints
     seg_fit <-
       suppressWarnings(segmented(
         fit,
         seg.Z = ~ trt,
+        # Starting point is 22 %VWC according to Hoover et al. 2014 this is a reasonable assumption
         psi = 22,
         # Only one breakpoint, for simplicity
         npsi = 1,
         # Bootstrap params
         control = seg.control(n.boot = 50, tol = 1e-07)
       ))
+    
+    # Print to console for convenience
     print(seg_fit)
+    
+    # If no breakpoint detected, return zeroes
     if (is.null(seg_fit$psi)) {
       return(c(0, 0, 0))
     } else {
@@ -40,7 +48,10 @@ cycle_genotypes <-
     # infile: subpath of input file
     # response: string or vector of strings indicating traits on which to perform
     # breakpoint analysis
+    
+    # Iterate through responses
     for (resp in response) {
+      # Iterate through genotypes
       for (i in 1:3) {
         df <-
           read.csv(infile, header = T)
@@ -53,28 +64,47 @@ cycle_genotypes <-
           breakpoints <- get_breakpoints(df_one_geno, resp)
         }
       }
+      
       breakpoints <- as.data.frame(breakpoints)
+      # Needed in case a 3x3 matrix of zeros with no colnames is made!
+      # 
       colnames(breakpoints) <-
-        c("Initial", "Est.", "St.Err") # Needed in case a 3x3 matrix of zeros with no colnames is made!
+        c("Initial", "Est.", "St.Err")
+      
+      # Add genotype indicator
       breakpoints$geno <- c(11, 2, 5)
+      
+      # Add columns for the response variable and plant growth subset
       breakpoints$measure <- c(rep(resp, 3))
       breakpoints$subset <- c(rep(subset, 3))
+      
+      # Combine everything
       if (resp != response[1]) {
         breakpoints_df <- rbind(breakpoints_df, breakpoints)
       } else {
         breakpoints_df <- breakpoints
       }
     }
+    
+    # If error is higher than 3, it's probably not a real breakpoint
     breakpoints_df[(breakpoints_df$St.Err > 3), ]$Est. <- 0
+    
+    # Rename row names
     rownames(breakpoints_df) <- seq(1, nrow(breakpoints_df))
+    
     return(breakpoints_df)
   }
 
 
 run_breakpoint_analysis <-
   function() {
+    # This function runs breakpoint analysis on subsets of responses and
+    # writes them to file for future use in plotting, etc.
+    
     df <-
       rbind(
+        
+        # Recovery subset
         cycle_genotypes(
           infile = "data/recovery_plants_clean.csv",
           subset = "Recovery",
@@ -89,6 +119,8 @@ run_breakpoint_analysis <-
             "A_B"
           )
         ),
+        
+        # Cumulative subset
         cycle_genotypes(
           infile = "data/biomass_plants_clean.csv",
           subset = "Cumulative",
@@ -110,6 +142,8 @@ run_breakpoint_analysis <-
             "A_B"
           )
         ),
+        
+        # Instantaneous subset
         cycle_genotypes(
           infile = "data/phys_plants_clean.csv",
           subset = "Instantaneous",
@@ -124,6 +158,8 @@ run_breakpoint_analysis <-
             "max_Anet"
           )
         ),
+        
+        # Growth subset
         cycle_genotypes(
           infile = "data/all_plants_clean.csv",
           subset = "Growth",
